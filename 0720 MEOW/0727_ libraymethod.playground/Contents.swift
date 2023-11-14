cca371829d73ed40217a9e63ced498e98e32eb00
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2023-11-14 19:50:08 +0800

    Add files via upload

---
+++ 0720 MEOW/0727_ libraymethod.playground/Contents.swift
@@ -0,0 +1,41 @@
+
+3 + 2 - 1
+5 * 4
+6 / 4
+6 % 4
+2.5 * 3
+7 + 2 * 3
+( 7 + 2 ) * 3
+
+100 * 15  * 40 * 50
+5 * 0.6214
+
+
+52 / ( 1.5 * 1.5 )
+
+"Peter Pan"
+
+"1" + "1"
+
+1 + 1
+
+// math grade
+98
+// english grade
+90
+// average
+(98 + 90) / 2
+
+/*
+  math 99, english 90
+ calculate together
+ */
+( 98 + 90 ) / 2
+
+let number = 10 * 10
+let height = 150
+
+let numberOfDogs = 10
+let numberOfCats = 10
+let totalNumbers = numberOfCats * numberOfDogs
+
---
+++ 0720 MEOW/MEOW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MEOW
+//
+//  Created by yucian huang on 2023/7/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
